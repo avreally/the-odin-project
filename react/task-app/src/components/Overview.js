122e71ae03abd3839785abb2d5f6c17603bc1e3c
Valeria-Shadrina <waleria.shadrina@gmail.com>
2021-07-23 14:54:58 +0200

    Added icons to buttons

--- react/task-app/src/components/Overview.js
+++ react/task-app/src/components/Overview.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { FaRegTrashAlt } from "react-icons/fa";
+import { FaRegEdit } from "react-icons/fa";
 
 const Task = ({ task, handleDelete }) => {
   return (
     <div>
       <p>
-        {task.name} <button onClick={() => handleDelete(task)}>delete</button>
+        {task.name}{" "}
+        <button>
+          <FaRegEdit />
+        </button>{" "}
+        <button onClick={() => handleDelete(task)}>
+          <FaRegTrashAlt />
+        </button>
       </p>
     </div>
   );
