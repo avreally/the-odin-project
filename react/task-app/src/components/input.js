dfe4aae74b684f8009c66e8898b3b8a9a53c0f10
Valeria-Shadrina <waleria.shadrina@gmail.com>
2021-09-16 20:34:05 +0200

    Added css to Input component

--- react/task-app/src/components/input.js
+++ react/task-app/src/components/input.js
@@ -1,15 +1,18 @@
 import React from "react";
+import "../styles/input.css";
 
 const Input = ({ addTask, newTask, handleInputChange }) => {
   return (
-    <form onSubmit={addTask}>
+    <form onSubmit={addTask} className="new-task-form">
       <label htmlFor="newTaskInput">Create new task:</label>
-      <input
-        id="newTaskInput"
-        value={newTask}
-        onChange={handleInputChange}
-      ></input>
-      <button type="submit">submit</button>
+      <div className="input-and-button">
+        <input
+          id="newTaskInput"
+          value={newTask}
+          onChange={handleInputChange}
+        ></input>
+        <button type="submit">submit</button>
+      </div>
     </form>
   );
 };
